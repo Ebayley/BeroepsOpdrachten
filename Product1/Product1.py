3446cf4496cf6d3c8fa7f9e64edbe3d038729cc7
Elona Bayley <28581@ma-web.nl>
2020-09-11 14:29:54 +0200

    add files

---
+++ Product1/Product1.py
@@ -0,0 +1,25 @@
+print("Hello You!, ik ben Elona")
+name = input("Wie ben jij? ")
+print("Hello " + name)
+import datetime
+t = datetime.datetime.now()
+print("De datum en tijd is: " + t.strftime("%c"))
+print(name + "wil jij dit programma nog een keer doen? Type Y/N")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+64 32 16 8 4 2 1
---
+++ Product1/Pruduct1.py
@@ -0,0 +1,6 @@
+print("Hello You!, ik ben Elona")
+name = input("Wie ben jij? ")
+print("Hello " + name)
+import datetime
+t = datetime.datetime.now()
+print("De datum en tijd is" + t.strftime("%c"))
